04083861eb76f9c16b1741d9db07f00ab346dc93
ramunarlapati-13 <ramunarlapati@gmail.com>
2026-01-25 23:18:41 +0530

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,5 +1,4 @@
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
@@ -13,6 +12,5 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-export const analytics = getAnalytics(app);
 export const auth = getAuth(app);
 export default app;
